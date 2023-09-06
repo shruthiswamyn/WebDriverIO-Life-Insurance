65c1483d41e0e2f3ac638bdf5869b702793d78eb
shruthiswamyn <shruthi.swamy@gmail.com>
2023-09-06 17:25:24 +0530

    commit more

--- test/specs/demo.js
+++ test/specs/demo.js
@@ -7,7 +7,7 @@ describe('Sample Testing', () => {
          //await expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO')
    
          await browser.url('https://www.google.com/')
-         await $('#hgdfh').click()//INTENTIONAL ERROR TO CHECK SCREESNHOT IN ALLURE REPORTS
+         //await $('#hgdfh').click()//INTENTIONAL ERROR TO CHECK SCREESNHOT IN ALLURE REPORTS
          const title = await browser.getTitle()
          console.log(title)
          expect(title).to.be.equal('Google')//when using chai assertion, using await before expect is not mandatory
